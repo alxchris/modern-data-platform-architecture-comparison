6643f490a6f499cb36f1fb81289962d67421aa97
alxchris <alxchris@hotmail.com>
2026-03-10 10:50:09 +0200

    Update silver_merge.sql

--- code/databricks/silver_merge.sql
+++ code/databricks/silver_merge.sql
@@ -1,1 +1,8 @@
-
+-- Databricks Silver validation / transformation example
+SELECT
+    status,
+    COUNT(*) AS cnt
+FROM case01_payments.silver_payments
+WHERE transaction_ts >= '2025-02-01'
+GROUP BY status
+ORDER BY cnt DESC;
