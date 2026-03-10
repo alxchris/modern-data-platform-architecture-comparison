f4912c7edf7c75c6669e9fea62d802525ab07242
alxchris <alxchris@hotmail.com>
2026-03-10 10:49:47 +0200

    Update bronze_ingestion.sql

--- code/databricks/bronze_ingestion.sql
+++ code/databricks/bronze_ingestion.sql
@@ -1,1 +1,6 @@
-
+-- Databricks Bronze validation
+SELECT
+    MIN(transaction_ts) AS min_transaction_ts,
+    MAX(transaction_ts) AS max_transaction_ts,
+    COUNT(*) AS total_rows
+FROM case01_payments.bronze_payments;
